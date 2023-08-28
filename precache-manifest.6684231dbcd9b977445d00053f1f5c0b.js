e33b13d40627b739f2d37484c1b6f0633c5e1db1
fahad79 <49613188+fahad79@users.noreply.github.com>
2023-08-28 17:23:08 +0530

    Add files via upload

---
+++ precache-manifest.6684231dbcd9b977445d00053f1f5c0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d12df02171548cc461aa",
+    "url": "/TextUtils-React/static/css/main.44544988.chunk.css"
+  },
+  {
+    "revision": "d12df02171548cc461aa",
+    "url": "/TextUtils-React/static/js/main.d12df021.chunk.js"
+  },
+  {
+    "revision": "0be899e96697b337e691",
+    "url": "/TextUtils-React/static/js/runtime~main.0be899e9.js"
+  },
+  {
+    "revision": "1688f0b2bca45a7c054a",
+    "url": "/TextUtils-React/static/js/2.1688f0b2.chunk.js"
+  },
+  {
+    "revision": "8b2c6b73b63048acb37535cc11902d72",
+    "url": "/TextUtils-React/index.html"
+  }
+];
